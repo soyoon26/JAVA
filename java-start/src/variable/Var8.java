fe74e2dbfa2ac847a5773d9e754d41e76227d635
soyoon26 <barcardi26@gmail.com>
2024-03-06 22:27:39 +0900

    basic JAVA

---
+++ java-start/src/variable/Var8.java
@@ -0,0 +1,16 @@
+package variable;
+
+public class Var8 {
+    public static void main(String[] args) {
+        byte b = 127; // -128~ 127
+        short s = 32767; // -32768 ~ 32767
+        int i = 2147438647; //-2147483648 ~ 2147483647
+        // long l = 92~~l;
+
+        //실수
+        float f = 10.0f;
+        double d = 10.0;
+        
+        //상수는 대문자, 패키지는 소문자
+    }
+}
