a2bb03c7ef8bfb9593f125e3ee12578aacec3cea
soyoon26 <barcardi26@gmail.com>
2024-03-07 15:08:32 +0900

    basic problem

---
+++ java-start/src/variable/ex/VarEx3.java
@@ -0,0 +1,10 @@
+package variable.ex;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long longVar = 10000000000L; //L을 넣어야 함
+        boolean or = true;
+        System.out.println(longVar); //soutv 가까운 값이 들어옴
+        System.out.println(or);
+    }
+}
