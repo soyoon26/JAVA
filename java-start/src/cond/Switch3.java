2bdedd3ac2eb0fbe1b008a6473694a6eacd6795f
soyoon26 <barcardi26@gmail.com>
2024-03-15 10:50:42 +0900

    study switch

---
+++ java-start/src/cond/Switch3.java
@@ -0,0 +1,22 @@
+package cond;
+
+public class Switch3 { //값이 같은지만 확인할 수 있음
+    public static void main(String[] args) {
+        int grade = 2;
+
+        int coupon;
+        switch (grade) {
+            case 1:
+                coupon = 1000;
+                break;
+            case 2:
+
+            case 3:
+                coupon = 3000;
+                break;
+            default:
+                coupon = 500;
+        }
+        System.out.println("발급받은 쿠폰 : "+coupon);
+    }
+}
