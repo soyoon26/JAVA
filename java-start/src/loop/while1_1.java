1d1c8510c44c51c198c20e2876e83328a6bcba50
soyoon26 <barcardi26@gmail.com>
2024-03-17 21:01:29 +0900

    loop_while

---
+++ java-start/src/loop/while1_1.java
@@ -0,0 +1,13 @@
+package loop;
+
+public class while1_1 {
+    public static void main(String[] args) {
+        int count = 0;
+        count = count + 1;
+        System.out.println("현재 숫자는 : " + count);
+        count = count + 1;
+        System.out.println("현재 숫자는 : " + count);
+        count = count + 1;
+        System.out.println("현재 숫자는 : " + count);
+    }
+}
